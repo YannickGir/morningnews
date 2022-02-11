a2254c77621632990217deded543bf10ca61d3e1
Lili Liardet <liardet.lili@gmail.com>
2022-02-11 12:27:38 +0100

    Schema

--- models/users.js
+++ models/users.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-    token: String,
-})
+  username: String,
+  email: String,
+  password: String,
+  token: String,
+  userArticles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'articles' }],
+});
 
-const userModel = mongoose.model('users', userSchema)
+const userModel = mongoose.model('users', userSchema);
 
-module.exports = userModel
+module.exports = userModel;
