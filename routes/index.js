ade8a9b72605765a29af5ddd4f87b32f2d8748e8
Lili Liardet <liardet.lili@gmail.com>
2022-02-11 11:07:30 +0100

    change route

--- routes/index.js
+++ routes/index.js
@@ -1,95 +1,83 @@
 var express = require('express');
 var router = express.Router();
 
-var uid2 = require('uid2')
+var uid2 = require('uid2');
 var bcrypt = require('bcrypt');
 
-var userModel = require('../models/users')
+var userModel = require('../models/users');
 
-
-router.post('/sign-up', async function(req,res,next){
-
-  var error = []
-  var result = false
-  var saveUser = null
-  var token = null
+router.post('/sign-up', async function (req, res, next) {
+  console.log('je suis Lili');
+  var error = [];
+  var result = false;
+  var saveUser = null;
+  var token = null;
 
   const data = await userModel.findOne({
-    email: req.body.emailFromFront
-  })
+    email: req.body.emailFromFront,
+  });
 
-  if(data != null){
-    error.push('utilisateur déjà présent')
+  if (data != null) {
+    error.push('utilisateur déjà présent');
   }
 
-  if(req.body.usernameFromFront == ''
-  || req.body.emailFromFront == ''
-  || req.body.passwordFromFront == ''
-  ){
-    error.push('champs vides')
+  if (
+    req.body.usernameFromFront == '' ||
+    req.body.emailFromFront == '' ||
+    req.body.passwordFromFront == ''
+  ) {
+    error.push('champs vides');
   }
 
-
-  if(error.length == 0){
-
+  if (error.length == 0) {
     var hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
     var newUser = new userModel({
       username: req.body.usernameFromFront,
       email: req.body.emailFromFront,
       password: hash,
       token: uid2(32),
-    })
-  
-    saveUser = await newUser.save()
-  
-    
-    if(saveUser){
-      result = true
-      token = saveUser.token
+    });
+
+    saveUser = await newUser.save();
+
+    if (saveUser) {
+      result = true;
+      token = saveUser.token;
     }
   }
-  
 
-  res.json({result, saveUser, error, token})
-})
+  res.json({ result, saveUser, error, token });
+});
 
-router.post('/sign-in', async function(req,res,next){
+router.post('/sign-in', async function (req, res, next) {
+  var result = false;
+  var user = null;
+  var error = [];
+  var token = null;
 
-  var result = false
-  var user = null
-  var error = []
-  var token = null
-  
-  if(req.body.emailFromFront == ''
-  || req.body.passwordFromFront == ''
-  ){
-    error.push('champs vides')
+  if (req.body.emailFromFront == '' || req.body.passwordFromFront == '') {
+    error.push('champs vides');
   }
 
-  if(error.length == 0){
+  if (error.length == 0) {
     user = await userModel.findOne({
       email: req.body.emailFromFront,
-    })
-  
-    
-    if(user){
-      if(bcrypt.compareSync(req.body.passwordFromFront, user.password)){
-        result = true
-        token = user.token
+    });
+
+    if (user) {
+      if (bcrypt.compareSync(req.body.passwordFromFront, user.password)) {
+        result = true;
+        token = user.token;
       } else {
-        result = false
-        error.push('mot de passe incorrect')
+        result = false;
+        error.push('mot de passe incorrect');
       }
-      
     } else {
-      error.push('email incorrect')
+      error.push('email incorrect');
     }
   }
-  
 
-  res.json({result, user, error, token})
-
-
-})
+  res.json({ result, user, error, token });
+});
 
 module.exports = router;
